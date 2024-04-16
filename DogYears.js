8c28d812b04d32e3ee20bc2b9eb423bf6b885dd4
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-04-17 05:05:29 +0900

    Add files via upload

---
+++ DogYears.js
@@ -0,0 +1,10 @@
+var myAge=41;
+let earlyYears=2;
+earlyYears = earlyYears * 10.5;
+let laterYears = myAge-2;
+laterYears*=4;
+console.log(earlyYears);
+console.log(laterYears);
+var myAgeInDogYears = earlyYears + laterYears;
+var myName ="Ryan".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)

0cc6aed99e37d3d8fb5be4e87b6edf37d91fba39
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-04-22 22:55:20 +0900

    Add files via upload

---
+++ 3.0 Javascript/8.4 CodeChallenges-reportingForDuty().js
@@ -0,0 +1,12 @@
+// Write your function here:
+const reportingForDuty = (rank, lastName) => {
+    return `${rank} ${lastName} reporting for duty!`;
+  };
+  
+  // Uncomment to test:
+  console.log(reportingForDuty("Private", "Fido"));
+  // Should return "Private Fido reporting for duty!"
+  
+  // We encourage you to add more function calls of your own to test your code!
+  console.log(reportingForDuty("Captain", "Mendoza"));
+  

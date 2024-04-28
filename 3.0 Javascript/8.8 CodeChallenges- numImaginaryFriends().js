a521a99eac3c456f7b5adb490fcc9122e2f600c9
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-04-28 22:49:49 +0900

    Update 8.8 CodeChallenges- numImaginaryFriends().js

--- 3.0 Javascript/8.8 CodeChallenges- numImaginaryFriends().js
+++ 3.0 Javascript/8.8 CodeChallenges- numImaginaryFriends().js
@@ -1,5 +1,12 @@
-const numImaginaryFriends = (friends) => {
-    let actualFriends = Math.ceil(friends * 0.25);
-    console.log(numImaginaryFriends(20));
-  };
-  
+const numImaginaryFriends = (claimedFriends) => 
+   Math.ceil(claimedFriends * 0.25);
+
+// Uncomment to test:
+console.log(numImaginaryFriends(20));
+// Should print 5
+
+console.log(numImaginaryFriends(10)) 
+// Should print 3 (2.5 rounded up!)
+
+// We encourage you to add more function calls of your own to test your code!
+console.log(numImaginaryFriends(3)) 

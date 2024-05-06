c48b3f74362f6538dfc128a78fa2622848cab49a
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-05-06 16:13:45 +0900

    Delete 3.0 Javascript directory

--- 3.0 Javascript/8.9 CodeChallenges-sillySentence().js
+++
@@ -1,8 +0,0 @@
-const sillySentence = (adjective, verb, noun) =>
-`I am so ${adjective} because I ${verb} coding! Time to write some more awesome ${noun}!`;
-
-// Uncomment to test:
-console.log(sillySentence("excited", "love", "functions"));
-
-// We encourage you to add more function calls of your own to test your code!
-console.log(sillySentence("grateful", "need", "code challenges"));

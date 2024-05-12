d4512b09d0ecac8ede7622105b42584ad0b625f4
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-05-13 00:11:58 +0900

    Update 16.0 WhaleTalk(in-progress).js

--- 3.0 JavaScript/16.0 WhaleTalk(in-progress).js
+++ 3.0 JavaScript/16.0 WhaleTalk(in-progress).js
@@ -3,6 +3,9 @@ const vowels = ["a", "e", "i", "o", "u"];
 const resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
-  
+  for (let v = 0; v < vowels.length; v++) {
+    if (input[i] === vowels[v]) {
+      console.log(`${i},${v}`);
+    }
+  }
 }
-console.log(input);

3d03876ce4bfb1faf4b2d6992c46885e9af910ac
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-05-13 01:24:45 +0900

    Update 16.0 WhaleTalk.js

--- 3.0 JavaScript/16.0 WhaleTalk(in-progress).js
+++ 3.0 JavaScript/16.0 WhaleTalk(in-progress).js
@@ -1,11 +1,20 @@
 let input = "Try and try until you die!";
-const vowels = ["a", "e", "i", "o", "u"];
-const resultArray = [];
+let vowels = ["a", "e", "i", "o", "u"];
+let resultArray = [];
 
 for (let i = 0; i < input.length; i++) {
   for (let v = 0; v < vowels.length; v++) {
     if (input[i] === vowels[v]) {
-      console.log(`${i},${v}`);
+    resultArray.push(input[i]);
     }
   }
+  if (input[i] === "e") {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === "u") {
+    resultArray.push(input[i]);
+  }
 }
+
+console.log(resultArray.join("").toUpperCase());
+   

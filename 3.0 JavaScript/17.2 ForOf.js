69b9faec674c6aa95eb500d9e7d6b227c555c573
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-08-09 03:51:08 +0900

    Delete 3.0 JavaScript/17.2 ForOf.js

--- 3.0 JavaScript/17.2 ForOf.js
+++
@@ -1,8 +0,0 @@
-// Write a for...of loop that iterates through our pokemonList array.
-const pokemonList = ['Pikachu', 'Charizard', 'Squirtle', 'Yoshi', 'Snorlax'];
-for (const pokemon of pokemonList) {
-  if (pokemon === "Yoshi") {
-    continue;
-  }
-  console.log(`You caught a ${pokemon}!`); 
-};

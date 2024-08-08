6b16e768d11a520e4fd7aea56b3a68320d795a79
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-08-09 06:08:22 +0900

    Update ArrowFunctionThis.js

--- 3.0 JavaScript/ArrowFunctionThis.js
+++ 3.0 JavaScript/ArrowFunctionThis.js
@@ -5,3 +5,14 @@ const robot = {
     }
   };
   console.log(robot.checkEnergy());
+
+//or 
+
+const robot = {
+    energyLevel: 100,
+    checkEnergy: function() {
+      console.log(`Energy is currently at ${this.energyLevel}%.`)
+    }
+  };
+  
+  robot.checkEnergy();

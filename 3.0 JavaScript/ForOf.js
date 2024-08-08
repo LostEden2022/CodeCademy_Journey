2922ba6321daa855f1b542f73c8cacb805cc9d90
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-08-09 03:50:37 +0900

    Update ForOf.js

--- 3.0 JavaScript/ForOf.js
+++ 3.0 JavaScript/ForOf.js
@@ -3,3 +3,12 @@ const spellingWord = 'hippopotamus';
 for (const char of spellingWord) {
   console.log(char);
 }
+
+// Write a for...of loop that iterates through our pokemonList array.
+const pokemonList = ['Pikachu', 'Charizard', 'Squirtle', 'Yoshi', 'Snorlax'];
+for (const pokemon of pokemonList) {
+  if (pokemon === "Yoshi") {
+    continue;
+  }
+  console.log(`You caught a ${pokemon}!`); 
+};

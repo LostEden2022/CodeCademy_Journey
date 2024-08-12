6e037d2f6407e52e73e5713719be03f6e930f028
Lost3d3n <154443968+LostEden2022@users.noreply.github.com>
2024-08-13 04:38:56 +0900

    Update MagicEightBall.js

--- 3.0 JavaScript/MagicEightBall.js
+++ 3.0 JavaScript/MagicEightBall.js
@@ -27,3 +27,44 @@ if (randomNumber === 0) {
   eightBall = "Try asking Again!";
 }
 console.log(`The Magic Eight Ball says: ${eightBall}!`);
+
+//or 
+const userName = "Ryan";
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello, stranger!");
+const userQuestion = "Are we experiencing an economic recession?";
+console.log(`${userName} asked: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+//Using switch/case/break statements:
+let eightBall = "";
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidedly so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My sources say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+  default:
+    eightBall = "Try asking Again";
+}
+console.log(`The Magic Eight Ball says: ${eightBall}!`);
+
